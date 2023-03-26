47f1fb34aadbfd2ea7a45082b577a48331f9daa2
Alex Sherman <asherman.ca@gmail.com>
2023-03-25 22:35:20 -0700

    login buttons on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { SignIn, SignInButton } from "@clerk/nextjs";
+import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
@@ -8,6 +8,8 @@ import { api } from "~/utils/api";
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
+  const user = useUser();
+
   return (
     <>
       <Head>
@@ -16,7 +18,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <SignInButton />
+        {!user.isSignedIn ? <SignInButton /> : <SignOutButton />}
       </main>
     </>
   );
